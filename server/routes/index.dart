5ebb4bb6a4c59b42af00643af63c56ad06b31238
ushieru <uzielcocolan@gmail.com>
2022-10-09 18:01:39 -0500

    first commit

---
+++ server/routes/index.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+import 'package:dart_frog/dart_frog.dart';
+import 'package:path/path.dart' as path;
+
+Response onRequest(RequestContext context) {
+  final file = File(path.join(Directory.current.path, 'public', 'index.html'));
+  if (!file.existsSync()) {
+    return Response(body: 'Index Not found');
+  }
+  final indexHtml = file.readAsStringSync();
+  return Response(body: indexHtml, headers: {'Content-Type': 'text/html'});
+}
